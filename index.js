ae89bb6ea780bfb2a14c33c839be93be1a606d9d
shubham43MP <shubhamdixit19950321@gmail.com>
2022-01-07 19:40:06 +0530

    Added clustering code

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const express = require("express");
-const app = express();
+const cluster = require("cluster");
 
-function doWork(duration) {
-  const start = Date.now();
-  while (Date.now() - start < duration) {}
+if (cluster.isMaster) {
+  cluster.fork(); // With only one cluster.fork(), the server may hangup if "/" route is called
+  cluster.fork();
+  // cluster.fork();
+} else {
+  // Child Process
+  const express = require("express");
+  const app = express();
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get("/", (req, res) => {
+    doWork(5000); // CAN BASICALLY BLOCK THE SERVER TO ACCEPT REQUEST
+    res.send("Hi There");
+  });
+
+  app.get("/fast", (req, res) => {
+    res.send("This was fast");
+  });
+
+  app.listen(3000);
 }
-
-app.get("/", (req, res) => {
-  doWork(5000); // CAN BASICALLY BLOCK THE SERVER TO ACCEPT REQUEST
-  res.send("Hi There");
-});
-
-app.listen(3000);
